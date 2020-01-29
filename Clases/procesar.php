8230d55cb433e8e9c50652d9a498a76acf89db49
Helmer Yonnich Salas Francisco <h.salas@sisalril.gov.do>
2020-01-29 16:50:10 -0400

    Agregacion de conexion, bootstrap y clase para procesar los manuales

---
+++ Clases/procesar.php
@@ -0,0 +1,38 @@
+<?php
+
+class Procesar{
+
+  public function __construct(){}
+
+
+public function BuscarManual($Nombre){
+
+}
+
+
+
+
+  public function InsertarManual(){
+
+  }
+
+
+public function ActualizarManual(){
+  
+}
+
+public function EliminarManual(){
+
+}
+
+
+
+
+
+
+
+}
+
+
+
+?>
